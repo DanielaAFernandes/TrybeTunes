f9c55361de0c380e6f603a41d94e14113ffcc610
Daniela <daniela@pop-os.localdomain>
2022-11-03 17:36:36 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import Search from './Search';
+import Album from './Album';
+import Favorites from './Favorites';
+import Profile from './Profile';
+import NotFound from './NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Switch>
+          <div>
+            <h1>Minha homepage</h1>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="/" component={ NotFound } />
+          </div>
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
-// import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from './services/userAPI';
 
 class Login extends Component {
+  state = {
+    loginName: '',
+    isSaveButtonDisabled: true,
+    loading: false,
+    userLogin: false,
+  };
+
+  validateButton = () => {
+    const { loginName } = this.state;
+    const minNumber = 3;
+    return loginName.length >= minNumber;
+  };
+
+  enableButton = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({
+        isSaveButtonDisabled: !this.validateButton(),
+      });
+    });
+  };
+
+  isLoadingTrue = () => {
+    this.setState({ loading: true }, async () => {
+      const { loginName } = this.state;
+      await createUser({ name: loginName });
+      this.setState({
+        userLogin: true,
+        loading: false,
+      });
+    });
+  };
+
   render() {
+    const { loginName, isSaveButtonDisabled, loading, userLogin } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        { loading
+          ? <Loading />
+          : (
+            <form>
+              <label htmlFor="login-name-input">
+                Insira seu nome
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  name="loginName"
+                  value={ loginName }
+                  onChange={ this.enableButton }
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ isSaveButtonDisabled }
+                onClick={ this.isLoadingTrue }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        { userLogin && <Redirect to="/search" />}
+      </div>
     );
   }
 }
+
+// Login.propTypes = {
+//   loginName: PropTypes.string.isRequired,
+//   isSaveButtonDisabled: PropTypes.func.isRequired,
+//   onInputChange: PropTypes.func.isRequired,
+// };
 
 export default Login;
