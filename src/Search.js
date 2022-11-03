81166b4acbc2626195c9c895c68d76115f339d92
Daniela <daniela@pop-os.localdomain>
2022-11-03 19:49:02 -0300

    Requisito 5

--- src/Search.js
+++ src/Search.js
@@ -3,10 +3,53 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  state = {
+    artistName: '',
+    disabledButton: true,
+  };
+
+  validateSearchButton = () => {
+    const { artistName } = this.state;
+    const minNumber = 2;
+    return artistName.length >= minNumber;
+  };
+
+  enableSearchButton = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({
+        disabledButton: !this.validateSearchButton(),
+      });
+    });
+  };
+
   render() {
+    const { artistName, disabledButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="login-name-input">
+            Insira o nome do artista/banda
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="artistName"
+              value={ artistName }
+              onChange={ this.enableSearchButton }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disabledButton }
+            // onClick={ this.isLoadingTrue }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
